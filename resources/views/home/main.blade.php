13cd6a9379f8848c4a69ba8e26479277c615f4bc
Nico <nicos.t.65@gmail.com>
2024-02-09 16:35:31 +0700

    init commit

---
+++ resources/views/home/main.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>In Between Stories</title>
+
+    {{-- Tailwind CSS --}}
+    <script src="https://cdn.tailwindcss.com/3.3.0"></script>
+
+    {{-- jQuery --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <script>
+        tailwind.config = {
+            darkMode: "class",
+            theme: {},
+            corePlugins: {
+                preflight: false,
+            },
+        };
+    </script>
+
+    <style>
+        body {
+            font-family: 'system-ui', sans-serif;
+            overflow-x: hidden;
+            margin: 0;
+            padding: 0;
+            background-color: lightskyblue;
+        }
+
+        ::-webkit-scrollbar {
+            width: 0;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: transparent;
+        }
+
+        ::-webkit-scrollbar-track {
+            width: 0;
+            background-color: transparent;
+        }
+    </style>
+
+    <link rel="stylesheet" href="{{ asset('/style/phase1.css') }}">
+</head>
+
+<body>
+    <main class="w-screen h-fit">
+        @include('home.phase1')
+    </main>
+
+    <script src="{{ asset('/script/home.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/home/phase1.blade.php
@@ -0,0 +1,13 @@
+<section id="phase1" class="relative">
+    <img src="{{ asset('/assets/home/phase1/background.png') }}" alt="Phase 1 BG" id="phase1_bg" loading="eager"
+        class="w-screen z-[1]">
+
+    <img src="{{ asset('/assets/home/phase1/coffee.png') }}" alt="Coffee Mug" id="phase1_mug"
+        class="absolute top-[175px] left-[490px] z-[3]" loading="eager" height="1025px">
+
+    <img src="{{ asset('/assets/home/phase1/coffee_pour.png') }}" alt="Coffee Pour" id="phase1_pour"
+        class="absolute top-[-460px] left-[725px] z-[2]" loading="eager" height="900px">
+
+    <img src="{{ asset('/assets/home/phase1/clock.png') }}" alt="Clock" id="phase1_clock"
+        class="absolute top-[800px] left-[1030px] z-[4]" loading="eager" height="600px">
+</section>
