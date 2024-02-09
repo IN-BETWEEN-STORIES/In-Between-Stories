88dd992fb9c336bedcab646028ba577683b5445b
Nico <nicos.t.65@gmail.com>
2024-02-09 20:32:39 +0700

    feat: responsive, make draggable element

--- resources/views/home/phase1.blade.php
+++ resources/views/home/phase1.blade.php
@@ -1,13 +1,13 @@
-<section id="phase1" class="relative">
+<section id="phase1" class="relative w-full">
     <img src="{{ asset('/assets/home/phase1/background.png') }}" alt="Phase 1 BG" id="phase1_bg" loading="eager"
         class="w-screen z-[1]">
 
     <img src="{{ asset('/assets/home/phase1/coffee.png') }}" alt="Coffee Mug" id="phase1_mug"
-        class="absolute top-[175px] left-[490px] z-[2]" loading="eager" height="1025px">
+        class="absolute w-[60%] top-[6%] left-[35%] z-[2]" loading="eager">
 
     <img src="{{ asset('/assets/home/phase1/coffee_pour.png') }}" alt="Coffee Pour" id="phase1_pour"
-        class="absolute top-[-460px] left-[725px] z-[3]" loading="eager" height="900px">
+        class="absolute w-[8.5%] top-[-15%] left-[52.5%] z-[3]" loading="eager">
 
     <img src="{{ asset('/assets/home/phase1/clock.png') }}" alt="Clock" id="phase1_clock"
-        class="absolute top-[800px] left-[1030px] z-[4]" loading="eager" height="600px">
+        class="absolute w-[25%] top-[24.25%] left-[72.5%] z-[4] cursor-grab" loading="eager">
 </section>
