84d7549fcae28135f1a6443907c38485a9c13a78
Darrell Cornelius Rivaldo <darrellcornelius37@gmail.com>
2024-02-24 14:13:38 +0700

    feat: grouping some elements & nambah anting2

--- resources/views/home/phase1.blade.php
+++ resources/views/home/phase1.blade.php
@@ -18,81 +18,99 @@
     <img src="{{ asset('/assets/home/new_phase1/tanaman_depan_tangan_kopi.png') }}" alt="Tanaman Depan"
         id="phase1_tanaman_depan" class="absolute w-[30%] top-[25.75%] right-0 z-[3]" loading="eager">
 
-    {{-- Cangkir belakang --}}
-    <img src="{{ asset('/assets/home/new_phase1/kopi_belakang.png') }}" alt="Kopi" id="phase1_kopi_belakang"
-        class="absolute w-[44%] top-[2.5%] left-[34.5%] z-[3]" loading="eager">
+    {{-- Foto kirine jam --}}
+    <img src="{{ asset('/assets/home/new_phase1/maureen.png') }}" alt="Foto Kiri" id="phase1_foto_kiri"
+        class="absolute w-[51.5%] top-[25.75%] left-[20.5%] z-[4]" loading="lazy">
 
-    {{-- Coffee Pour --}}
-    <img src="{{ asset('/assets/home/new_phase1/coffe_pour.png') }}" alt="Coffee Pour" id="phase1_pour"
-        class="absolute w-[8.5%] top-[-16.75%] left-[49%] z-[4]" loading="eager">
+    {{-- cangkir & tangannya --}}
+    <div>
+        {{-- Cangkir belakang --}}
+        <img src="{{ asset('/assets/home/new_phase1/kopi_belakang.png') }}" alt="Kopi" id="phase1_kopi_belakang"
+            class="absolute w-[44%] top-[2.5%] left-[34.5%] z-[3]" loading="eager">
+        {{-- Coffee Pour --}}
+        <img src="{{ asset('/assets/home/new_phase1/coffe_pour.png') }}" alt="Coffee Pour" id="phase1_pour"
+            class="absolute w-[8.5%] top-[-16.75%] left-[49%] z-[4]" loading="eager">
+        {{-- Cangkir depan --}}
+        <img src="{{ asset('/assets/home/new_phase1/kopi_depan.png') }}" alt="Kopi" id="phase1_kopi_depan"
+            class="absolute w-[44%] top-[2.5%] left-[34.5%] z-[5]" loading="eager">
+        {{-- Tangan megang kopi --}}
+        <img src="{{ asset('/assets/home/new_phase1/tangan_kopi.png') }}" alt="Kopi" id="phase1_tangan_kopi"
+            class="absolute w-[67%] top-[8.25%] left-[31.5%] z-[5]" loading="eager">
+    </div>
 
-    {{-- Cangkir depan --}}
-    <img src="{{ asset('/assets/home/new_phase1/kopi_depan.png') }}" alt="Kopi" id="phase1_kopi_depan"
-        class="absolute w-[44%] top-[2.5%] left-[34.5%] z-[5]" loading="eager">
+    {{-- anting2 --}}
+    <div>
+        {{-- anting kiri --}}
+        <img src="{{ asset('/assets/home/new_phase1/anting1.png') }}" alt="Anting Kiri" id="anting_kiri"
+            class="absolute w-[9.6%] top-[24.15%] left-[55.15%] z-[4]">
+        {{-- anting kanan --}}
+        <img src="{{ asset('/assets/home/new_phase1/anting2.png') }}" alt="Anting Kanan" id="anting_kanan"
+            class="absolute w-[9.35%] top-[27.35%] left-[62.03%] z-[4]">
+    </div>
 
-    {{-- Tangan megang kopi --}}
-    <img src="{{ asset('/assets/home/new_phase1/tangan_kopi.png') }}" alt="Kopi" id="phase1_tangan_kopi"
-        class="absolute w-[67%] top-[8.25%] left-[31.5%] z-[5]" loading="eager">
+    {{-- isi laci atas --}}
+    <div>
+        {{-- Laci 1 --}}
+        <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 1" id="phase1_laci_1"
+            class="absolute w-[79.5%] top-[44.46%] right-[-9.5%] z-[6]" loading="lazy">
 
-    {{-- Jam --}}
-    <img src="{{ asset('/assets/home/new_phase1/jam.png') }}" alt="Jam" id="phase1_jam"
-        class="absolute w-[31.75%] top-[21.75%] left-[68.5%] z-[6]" loading="lazy">
+        {{-- Jam --}}
+        <img src="{{ asset('/assets/home/new_phase1/jam.png') }}" alt="Jam" id="phase1_jam"
+            class="absolute w-[31.75%] top-[21.75%] left-[68.5%] z-[6]" loading="lazy">
 
-    {{-- Jarum jam --}}
-    <img src="{{ asset('/assets/home/new_phase1/jarum_jam.png') }}" alt="Jam" id="phase1_jarum_jam"
-        class="absolute w-[8.5%] top-[30.6%] left-[82.5%] z-[6]" loading="lazy">
+        {{-- Jarum jam --}}
+        <img src="{{ asset('/assets/home/new_phase1/jarum_jam.png') }}" alt="Jam" id="phase1_jarum_jam"
+            class="absolute w-[8.5%] top-[30.6%] left-[82.5%] z-[6]" loading="lazy">
 
-    {{-- Jarum jam 2 --}}
-    <img src="{{ asset('/assets/home/new_phase1/jarum_jam_2.png') }}" alt="Jam" id="phase1_jarum_jam_2"
-        class="absolute w-[12.5%] top-[29.75%] left-[80.5%] z-[6]" loading="lazy">
+        {{-- Jarum jam 2 --}}
+        <img src="{{ asset('/assets/home/new_phase1/jarum_jam_2.png') }}" alt="Jam" id="phase1_jarum_jam_2"
+            class="absolute w-[12.5%] top-[29.75%] left-[80.5%] z-[6]" loading="lazy">
 
-    {{-- Jarum jam 3 --}}
-    <img src="{{ asset('/assets/home/new_phase1/jarum_jam_3.png') }}" alt="Jam" id="phase1_jarum_jam_2"
-        class="absolute w-[8.5%] top-[29.75%] left-[80.6%] z-[6]" loading="lazy">
+        {{-- Jarum jam 3 --}}
+        <img src="{{ asset('/assets/home/new_phase1/jarum_jam_3.png') }}" alt="Jam" id="phase1_jarum_jam_2"
+            class="absolute w-[8.5%] top-[29.75%] left-[80.6%] z-[6]" loading="lazy">
 
-    {{-- Jarum jam 4 --}}
-    <img src="{{ asset('/assets/home/new_phase1/jarum_jam_4.png') }}" alt="Jam" id="phase1_jarum_jam_2"
-        class="absolute w-[10%] top-[32.1%] left-[77.5%] z-[6]" loading="lazy">
+        {{-- Jarum jam 4 --}}
+        <img src="{{ asset('/assets/home/new_phase1/jarum_jam_4.png') }}" alt="Jam" id="phase1_jarum_jam_2"
+            class="absolute w-[10%] top-[32.1%] left-[77.5%] z-[6]" loading="lazy">
+
+        {{-- Kertas laci 1 --}}
+        <img src="{{ asset('/assets/home/new_phase1/kertas_laci_1.png') }}" alt="Kertas Laci 1"
+            id="phase1_kertas_laci_1" class="absolute w-[25%] top-[38.5%] left-[55%] z-[5]" loading="lazy">
+    </div>
+
+    {{-- isi laci tengah --}}
+    <div>
+        {{-- Laci 2 --}}
+        <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 2" id="phase1_laci_2"
+            class="absolute w-[79.5%] top-[55.15%] right-[-9.5%] z-[8]" loading="lazy">
+        {{-- Kertas laci 2 --}}
+        <img src="{{ asset('/assets/home/new_phase1/kertas_laci_2.png') }}" alt="Laci" id="phase1_kertas_laci_2"
+            class="absolute w-[32%] top-[48%] left-[31%] z-[7]" loading="lazy">
+    </div>
+
+    {{-- isi laci bawah --}}
+    <div>
+        {{-- Laci 3 --}}
+        <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 3" id="phase1_laci_3"
+            class="absolute w-[79.5%] top-[65.84%] right-[-9.5%] z-[11]" loading="lazy">
+        {{-- Kertas laci 3 / kertas --}}
+        <img src="{{ asset('/assets/home/new_phase1/kertas_laci_3.png') }}" alt="Kertas Laci 3"
+            id="phase1_kertas_laci_3" class="absolute w-[33%] top-[59.02%] left-[66%] z-[9]" loading="lazy">
+        {{-- Kertas laci 4 / bunga --}}
+        <img src="{{ asset('/assets/home/new_phase1/kertas_laci_4.png') }}" alt="Bunga" id="phase1_kertas_laci_4"
+            class="absolute w-[24%] top-[59%] left-[75.1%] z-[10]" loading="lazy">
+    </div>
 
     {{-- Kertas jatuh 2 --}}
     <img src="{{ asset('/assets/home/new_phase1/kertas_jatuh_2.png') }}" alt="Kertas Jatuh 2"
         id="phase1_kertas_jatuh_2" class="absolute w-[26%] top-[29.25%] left-[13%] z-[3]" loading="lazy">
 
-    {{-- Foto kirine jam --}}
-    <img src="{{ asset('/assets/home/new_phase1/maureen.png') }}" alt="Foto Kiri" id="phase1_foto_kiri"
-        class="absolute w-[51.5%] top-[25.75%] left-[20.5%] z-[4]" loading="lazy">
-
     {{-- Koper & Lemari --}}
     <img src="{{ asset('/assets/home/new_phase1/koper_lemari.png') }}" alt="Koper & Lemari" id="phase1_koper_lemari"
         class="absolute w-[95%] top-[41.5%] right-0 z-[4]" loading="lazy">
 
-    {{-- Laci 1 --}}
-    <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 1" id="phase1_laci_1"
-        class="absolute w-[79.5%] top-[44.46%] right-[-9.5%] z-[6]" loading="lazy">
 
-    {{-- Kertas laci 1 --}}
-    <img src="{{ asset('/assets/home/new_phase1/kertas_laci_1.png') }}" alt="Kertas Laci 1" id="phase1_kertas_laci_1"
-        class="absolute w-[25%] top-[38.5%] left-[55%] z-[5]" loading="lazy">
-
-    {{-- Laci 2 --}}
-    <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 2" id="phase1_laci_2"
-        class="absolute w-[79.5%] top-[55.15%] right-[-9.5%] z-[8]" loading="lazy">
-
-    {{-- Kertas laci 2 --}}
-    <img src="{{ asset('/assets/home/new_phase1/kertas_laci_2.png') }}" alt="Laci" id="phase1_kertas_laci_2"
-        class="absolute w-[32%] top-[48%] left-[31%] z-[7]" loading="lazy">
-
-    {{-- Laci 3 --}}
-    <img src="{{ asset('/assets/home/new_phase1/laci.png') }}" alt="Laci 3" id="phase1_laci_3"
-        class="absolute w-[79.5%] top-[65.84%] right-[-9.5%] z-[11]" loading="lazy">
-
-    {{-- Kertas laci 3 / kertas --}}
-    <img src="{{ asset('/assets/home/new_phase1/kertas_laci_3.png') }}" alt="Kertas Laci 3"
-        id="phase1_kertas_laci_3" class="absolute w-[33%] top-[59.02%] left-[66%] z-[9]" loading="lazy">
-
-    {{-- Kertas laci 4 / bunga --}}
-    <img src="{{ asset('/assets/home/new_phase1/kertas_laci_4.png') }}" alt="Bunga" id="phase1_kertas_laci_4"
-        class="absolute w-[24%] top-[59%] left-[75.1%] z-[10]" loading="lazy">
 
 
 </section>
